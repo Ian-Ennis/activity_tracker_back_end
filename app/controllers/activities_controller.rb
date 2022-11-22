80afc0bd7d8f7649faf3741591c73dcec96191ca
Ian <ipatrickenn7@gmail.com>
2022-11-22 15:43:26 -0700

    Refactored signup and login

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,42 +2,50 @@ class ActivitiesController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
     def index
-        activity = Activity.all
+        puts "++++ Activities: in index"
+        puts "params:"
+        puts params
+        activity = Activity.find_by!(user_id: params[:user_id])
+        puts activity
         render json: activity, status: :ok
     end
 
     def show 
+        puts "++++ Activities: in show"
         render json: @activity
     end
 
     def create
-        activity = Activity.create!(activity_params)
-        render json: activity, status: :created
-    end
-
-    def update
-        activity = find_activity
-        activity.update(activity_params)
-        render json: activity
+        activity = Activity.create!(user_id: params[:user_id], date: params[:date], minutes: params[:minutes], notes: params[:notes])
+        if activity.valid?
+            render json: activity, status: :created
+        else
+            render json: {error: "Activity could not be created"}, status: :unprocessable_entity
+        end
     end
 
     def destroy
+        puts "++++ Activities: in destroy"
         activity = find_activity
         activity.destroy
         head :no_content
     end
 
     private
+    
+    def activity_params
+        puts "+++ Activities: in activity_params"
+        params.permit(:user_id, :date, :name, :yoga_type, :workout, :distance, :minutes, :notes)
+        puts "+++ Activities: leaving activity_params"
+    end
 
     def find_activity
+        puts "++++ Activities: in find_activity"
         Activity.find(params[:id])
     end
 
-    def activity_params
-        params.permit(:date, :name, :length, :minutes, :notes, :yoga_type, :workout, :distance)
-    end
-
     def render_not_found_response
+        puts "++++ Activities: in render_not_found_response"
         render json: { error: "Activity not found" }, status: :not_found
     end
 
